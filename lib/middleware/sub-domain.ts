36eafc2e5d7797277aab6cf2193f847f7a596b8c
Thien Do <5953369+thien-do@users.noreply.github.com>
2022-11-30 20:12:58 +0700

    test

--- lib/middleware/sub-domain.ts
+++ lib/middleware/sub-domain.ts
@@ -8,14 +8,19 @@ export const subDomainMiddleware: NextMiddleware = (request) => {
   const url = request.nextUrl.clone();
 
   const root = ROOT_DOMAINS.find((domain) => {
+    console.log("hostname", url.hostname);
     return url.hostname.endsWith(`.${domain}`);
   });
   if (root === undefined) return;
+
+  console.log({ root });
 
   // thien-do.memos.pub
   // ^------^ owner
   const owner = url.hostname.replace(`.${root}`, "");
   // "pathname" starts with "/"
+  console.log({ owner });
+  console.log("pathname", url.pathname);
   url.pathname = `/github/${owner}${url.pathname}`;
   return NextResponse.rewrite(url);
 };
