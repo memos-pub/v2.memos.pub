1cdb286bb2cba0e292f9ced44417ed9a5c090578
Thien Do <5953369+thien-do@users.noreply.github.com>
2022-12-05 21:15:35 +0700

    feat: Support multiple readme names

--- lib/blog/content/readme.ts
+++ lib/blog/content/readme.ts
@@ -3,22 +3,32 @@ import { getGithubContent, GitHubContent } from "#/lib/github/content";
 import { BlogPost } from "../post/type";
 import { parseBlogPost } from "../post/parse";
 import { BlogParams } from "../type";
+import { OctokitResponse } from "@octokit/types";
+
+const fetchReadmes = async (
+  params: BlogParams
+): Promise<GitHubContent | null> => {
+  const { owner, path: _path, repo } = params;
+
+  const promises = ["README.md", "readme.md"].map((file) => {
+    // Avoid leading "/" if at root (path === "")
+    const path = [_path, file].filter((p) => p !== "").join("/");
+    return getGithubContent({ owner, repo, path });
+  });
+  const responses = await Promise.allSettled(promises);
+  const success = responses.find((r) => r.status === "fulfilled") as
+    | undefined
+    | PromiseFulfilledResult<OctokitResponse<GitHubContent>>;
+  if (success === undefined) return null;
+  return success.value.data;
+};
 
 export const getBlogReadme = async (
   params: BlogParams
 ): Promise<BlogPost | null> => {
-  const { owner, repo } = params;
-  // Avoid leading "/" if at root (path === "")
-  const path = [params.path, "README.md"].filter((p) => p !== "").join("/");
-
-  let data: GitHubContent;
-  try {
-    const response = await getGithubContent({ owner, repo, path });
-    data = response.data;
-  } catch (error) {
-    // Maybe "path" is a file, or a folder without README
-    return null;
-  }
+  const data = await fetchReadmes(params);
+  // Maybe "path" is a file, or a folder without README
+  if (data === null) return null;
 
   if (Array.isArray(data)) {
     console.warn("readme is folder");
