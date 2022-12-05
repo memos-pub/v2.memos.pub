bdae0cfe4c6ea8b45262de4a15f7476c0de5b0dc
hà <52147754+violeine@users.noreply.github.com>
2022-12-05 21:37:44 +0700

    feat: Support heading links and meta tags (#7)
    Co-authored-by: Thien Do <5953369+thien-do@users.noreply.github.com>

--- lib/markdown/parse.ts
+++ lib/markdown/parse.ts
@@ -4,15 +4,20 @@ import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import "server-only";
 import { unified } from "unified";
-import { rehypeStarryNight } from "./rehype-starry-night";
+import { markdownCode } from "./code";
+import { markdownHeading } from "./heading";
+import { markdownMeta } from "./meta";
 
 export const parseMarkdown = async (raw: string): Promise<string> => {
   const compiler = unified()
     .use(remarkParse)
     .use(remarkGfm)
     .use(remarkRehype)
-    .use(rehypeStarryNight)
+    .use(markdownCode)
+    .use(markdownMeta)
+    .use(markdownHeading)
     .use(rehypeStringify);
+
   const result = await compiler.process(raw);
   const html = String(result);
 
